432da92f9b2e2c396a9450917ce8ccc25fd5885a
sungil yu <chinggin@naver.com>
2023-05-14 18:54:44 +0900

    Initial commit

---
+++ exampleBatch/src/main/java/com/example/examplebatch/ExampleBatchApplication.java
@@ -0,0 +1,13 @@
+package com.example.examplebatch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExampleBatchApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ExampleBatchApplication.class, args);
+    }
+
+}
