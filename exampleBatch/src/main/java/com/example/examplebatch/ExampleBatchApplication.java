5549cd9ec752f886bfdedac6222d2cf26a3e1fd1
sungil yu <chinggin@naver.com>
2023-05-21 15:26:16 +0900

    hello batch

--- exampleBatch/src/main/java/com/example/examplebatch/ExampleBatchApplication.java
+++ exampleBatch/src/main/java/com/example/examplebatch/ExampleBatchApplication.java
@@ -1,11 +1,12 @@
 package com.example.examplebatch;
 
+import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@EnableBatchProcessing
 public class ExampleBatchApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(ExampleBatchApplication.class, args);
     }
